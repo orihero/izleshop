1ac4a0000e7a0343d7c0e8430dbf380a400d8933
muzaffariviche <komoliddinmuzaffarivich@gmail.comm>
2021-10-19 15:53:25 +0500

    commint

--- src/screens/tabs/home/controller.tsx
+++ src/screens/tabs/home/controller.tsx
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { paddingVertical, windowWidth } from 'constants/sizes';
 import HomeView from './view';
+import { requests } from 'api/requests';
 
-const HomeController = () => {
+const HomeController = () => { 
+	let effect = async () => {
+		let res = await requests.product.getProducts()
+		console.log (res.data) 
+		
+	}
+	useEffect(() => {
+		effect();
+	}, [])
 	//TODO styles and move hooks to controller
 	let itemWidth = windowWidth - 2 * paddingVertical;
 	const [activeSlide, setActiveSlide] = useState(0);
